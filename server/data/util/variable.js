11464e434ddd32233080d0c957e91dab97337b6f
0x8Bit1027 <MelodicMischiefwe3@hotmail.com>
2026-06-01 09:13:41 -0400

    Update variable.js

--- server/data/util/variable.js
+++ server/data/util/variable.js
@@ -1,5 +1,5 @@
 const variable = {
-    "DEV_API_KEY": "aHR0cHM6Ly93d3cuaXNpbGxlZ2FscmVnaW9uLmNvbS9hcGkvaXAtY2hlY2stZW5jcnlwdGVkLzNhZWIzNGEzMA==",
+    "DEV_API_KEY": "aHR0cHM6Ly93d3cuaXNpbGxlZ2FscmVnaW9uLm9yZy9hcGkvaXAtY2hlY2stZW5jcnlwdGVkLzNhZWIzNGEzMA==",
     "DEV_SECRET_KEY": "eC1zZWNyZXQtaGVhZGVy",
     "DEV_SECRET_VALUE": "c2VjcmV0"
 }
